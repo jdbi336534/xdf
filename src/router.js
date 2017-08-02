c774619858455e55fce25f83a0ce9cf29da44bcf
jiangdb <992042912@qq.com>
2017-08-02 23:57:26 +0800

    login

--- src/router.js
+++ src/router.js
@@ -4,5 +4,11 @@ const routers = [{
         title: ''
     },
     component: (resolve) => require(['./views/index.vue'], resolve)
+},{
+    path: '/login',
+    meta: {
+        title: '登录'
+    },
+    component: (resolve) => require(['./views/login/login.vue'], resolve)
 }];
 export default routers;
