634638046ca965311d49aca6b0412003589ad9d7
jiangdb <jiangdb@r7data.com>
2017-08-21 17:15:07 +0800

    重新配置了mongodb服务器，更换了地址

--- xdf_server/config.js
+++ xdf_server/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     port:process.env.PORT || 3001,
     mongodb: {
-        url: 'mongodb://oliver:jdbi336534@123.207.153.145:27017/admin'
+        url: 'mongodb://oliver:jdbi336534@www.r7data.top:20000/admin'
     }
 }
